8b83ddd816fcd0725baad595dc4ac4af9fad01d3
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 16:28:41 +0300

    Add project files.

---
+++ C. Simple Operations/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+double num1 ,num2;
+num1=double.Parse(Console.ReadLine());
+num2=double.Parse(Console.ReadLine());
+Console.WriteLine(num1 + num2);
+Console.WriteLine(num1 - num2);
+Console.WriteLine(num1 * num2);
+Console.WriteLine(num1 / num2);
+
